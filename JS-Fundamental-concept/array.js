3a9f0e1859f74ee63e3d32be53c75364e16bdcdd
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-03-16 20:42:33 +0600

    added condition-js

---
+++ JS-Fundamental-concept/array.js
@@ -0,0 +1,19 @@
+ var vowels = ['a', 'e', 'i', 'o', 'u'];
+ console.log(vowels);
+ var vowelsCount = vowels.length;
+ console.log(vowelsCount);
+ // array index
+var number = [459, 465, 468, 66, 478, 22];
+var index =number.indexOf(66);
+console.log(index);
+var secondIndex = number[2];
+console.log(secondIndex);
+// array push
+var age = [17, 19, 12, 20, 14];
+    age.push(35);
+    console.log(age)
+// array pop
+age.pop();
+age.pop();
+age.pop();
+console.log(age);
---
+++ JS-Fundamental-concept/condition.js
@@ -0,0 +1,8 @@
+var potatoPrice = 10;
+var myBudget =18;
+if (myBudget > potatoPrice){
+    console.log('give me one')
+}
+else{
+    console.log('No, thanks')
+};
