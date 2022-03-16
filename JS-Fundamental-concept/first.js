8bb2aa4c0ab626010dbb1c105870bdc8dcbc4387
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-03-16 15:59:34 +0600

    first commit

---
+++ JS-Fundamental-concept/first.js
@@ -0,0 +1,13 @@
+var price = 17;
+console.log(price);
+var schoolName = "Talma gov't primary school";
+console.log(schoolName)
+var areayouOk = true;
+console.log(areayouOk)
+var age = 21;
+console.log(age+=8);
+console.log(17%5);
+
+var num1 = "99.5";
+var num2 = 15;
+console.log(num1+num2);
---
+++ JS-Fundamental-concept/mathematical-short-hand.js
@@ -0,0 +1,8 @@
+var eggPrice= 20;
+eggPrice += 8;
+eggPrice++;
+console.log(eggPrice);
+
+var orangePrice = 45;
+orangePrice--;
+console.log(orangePrice);
