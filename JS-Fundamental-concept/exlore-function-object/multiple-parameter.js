134c366ef0c0eaa52fe166416656563b41a3a0d1
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-03-18 12:20:24 +0600

    making function with multiple parameter

---
+++ JS-Fundamental-concept/exlore-function-object/multiple-parameter.js
@@ -0,0 +1,32 @@
+function addTwoNumber(num1, num2) {
+    console.log(num1, num2);
+    var total = num1 + num2;
+    return total;
+}
+var firstNum =21;
+var secondNum =29
+var result = addTwoNumber(firstNum, secondNum);
+console.log('result value', result);
+
+function multiplyTwonum(num1, num2) {
+    var result = num1 *num2;
+    return result;
+}
+var total = multiplyTwonum (55, 26);
+console.log('Your multiplication result is', total);
+
+function dividedTwoNum(num1, num2){
+    var result = num1 / num2;
+    return result;
+
+}
+var total = dividedTwoNum (500, 25);
+console.log('Your result is', total);
+
+function minusTwoNum (num1, num2) {
+    var result = num1 - num2;
+    return result;
+}
+var total = minusTwoNum (152, 33);
+console.log('Your minus result value is', total);
+
