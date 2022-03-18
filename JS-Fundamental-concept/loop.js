23664d9c2966089b3d1f89af671fd83de5fd0bb6
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-03-18 10:19:51 +0600

    practice some loop

---
+++ JS-Fundamental-concept/loop.js
@@ -0,0 +1,25 @@
+// while loop
+var  i =0;
+while(i<7){
+    console.log(i);
+    i++
+
+};
+// for llop
+for(var i =0; i<=7; i++){
+    console.log(i)
+};
+
+// loop from an array
+var number = [459, 465, 468, 66, 478, 22, 32, 56, 89];
+for(var i =0; i < number.length; i++){
+    var element = number[i];
+    console.log(element)
+};
+
+var items = ['mobile', 'watch', 'tablet', 'laptop', 'headphone', 'smart watch', 'mouse' ];
+for(var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item)
+};
+
