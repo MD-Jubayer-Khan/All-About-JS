4202c03e91f85da1a91ce715cdd5188931b010be
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-03-22 15:02:46 +0600

    explore some js math function

---
+++ apply-js-concept/math.js
@@ -0,0 +1,22 @@
+const myNumber = -25;
+const output = Math.abs(myNumber);
+console.log(output);
+
+// math celling
+const number = 25.6464;
+const ceilOutput = Math.ceil(number);
+console.log(ceilOutput);
+// math floor
+const num = 29.4133;
+const floorNum = Math.floor(num);
+console.log(floorNum);
+ 
+// math round
+const doRound = 15.2253;
+const roundNum = Math.round(doRound);
+console.log(roundNum);
+
+// math random
+const doRandom = Math.random() *55;
+const randomRounded = Math.round(doRandom);
+console.log(randomRounded); 
