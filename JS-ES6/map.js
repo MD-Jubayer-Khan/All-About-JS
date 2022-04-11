415c1647e61c831c1d2a5fb18d042563cb53b149
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-04-11 18:18:06 +0600

    explore js map

---
+++ JS-ES6/map.js
@@ -0,0 +1,10 @@
+const array = [5, 6, 15, 32, 10];
+const output = array.map(x => x * 2);
+console.log(output);
+const square = array.map(x => x * x);
+console.log(square);
+
+const friends = ['anik','asif', 'mahi', 'ovi', 'rakib', 'akash'];
+const fLength = friends.map(friend => friend.length);
+console.log(fLength);
+
