6cca784412f37d173fee44ac36f06018411370cc
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-04-11 12:05:58 +0600

    using template string in simple way

---
+++ JS-ES6/template-string.js
@@ -0,0 +1,8 @@
+ /* backcode (``) using declare multiline string that called template string
+ and dynamic string just need to declare ( ${} ) inside a template string */
+ const firstName = 'Hablu';
+ const lastName = 'Programmer';
+ const fullName = `This person name is : ${firstName} ${lastName}`;
+
+ console.log(fullName);
+
