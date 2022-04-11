7745072d30b192636b1375a123d37eb608e1e6fc
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-04-11 11:50:44 +0600

    start exploring javascript es6

---
+++ JS-ES6/let-const.js
@@ -0,0 +1,10 @@
+/* for fixed value to declare const
+const can't reassign */
+const hubby = 'Juliyet'
+console.log("You are my Romio I'm your", hubby);
+
+// if need to reassign the value then declare a let variable
+let myFriend = 'someone';
+    myFriend = 'Anik';
+    console.log(myFriend);
+    
