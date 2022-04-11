f120960d15c46793f2e804865e657a3a9b862eb4
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-04-11 20:34:16 +0600

    find large and small number using filter

---
+++ JS-ES6/filter.js
@@ -0,0 +1,8 @@
+const numbers = [12, 25, 121, 31, 13, 12, 52];
+const bigNumbers = numbers.filter( number => number > 50);
+console.log(bigNumbers);
+
+// find Small
+const smallNumber = numbers.filter( number => number < 50);
+console.log(smallNumber);
+
