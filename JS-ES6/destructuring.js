6c7b1fc39280d55bd901fa749c803a6144a844fe
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-04-11 12:45:54 +0600

    simple destructuring

---
+++ JS-ES6/destructuring.js
@@ -0,0 +1,15 @@
+const student = {
+    id: 115,
+    name : 'John',
+    marks: 81,
+    phone : +8801937013572,
+};
+/* const id = student.id;
+const name = student.name;
+const marks = student.marks;
+const phone = student.phone; */
+const {id, phone, name, marks} = student;
+console.log(id);
+console.log(phone);
+console.log(name);
+console.log(marks);
